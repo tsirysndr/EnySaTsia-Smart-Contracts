18367794f0ec49bf6c7de49c6d49c6a3dc50b9ba
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2018-12-29 16:45:06 +0300

    Add contract events

--- contracts/EnySaTsiaVoting.sol
+++ contracts/EnySaTsiaVoting.sol
@@ -11,6 +11,13 @@ contract EnySaTsiaVoting is Ownable {
   mapping(bytes32 => bool) public voteOpen;
   mapping(address => mapping(bytes32 => bytes)) public votes;
 
+  event StartVote(bytes32 indexed session, bytes32 indexed question);
+  event CloseVote(bytes32 indexed session, bytes32 indexed question);
+  event StartSession(bytes32 indexed session);
+  event CloseSession(bytes32 indexed session);
+  event Vote(bytes32 indexed session, bytes32 indexed question, bytes decision);
+  event CreateQuestion(bytes32 indexed session, bytes32 indexed question);
+
   constructor(bytes memory _name) public {
     name = _name;
   }
@@ -54,14 +61,17 @@ contract EnySaTsiaVoting is Ownable {
 
   function startSession() public onlyOwner {
     sessionOpen = true;
+    emit StartSession(sha256(name));
   }
 
   function closeSession() public onlyOwner {
     sessionOpen = false;
+    emit CloseSession(sha256(name));
   }
 
   function createQuestion(bytes memory question) public onlyOwner sessionIsActive validQuestion(question) {
     questions.push(question);
+    emit CreateQuestion(sha256(name), sha256(question));
   }
 
   function changeQuestion(bytes memory question, bytes memory sentense) public onlyOwner {
@@ -74,14 +84,17 @@ contract EnySaTsiaVoting is Ownable {
 
   function startVote(bytes memory question) public onlyOwner sessionIsActive {
     voteOpen[sha256(question)] = true;
+    emit StartVote(sha256(name), sha256(question));
   }
 
   function closeVote(bytes memory question) public onlyOwner sessionIsActive {
     voteOpen[sha256(question)] = false;
+    emit CloseVote(sha256(name), sha256(question));
   }
 
   function vote(bytes memory decision, bytes memory question) public sessionIsActive votingIsActive(question) {
     votes[msg.sender][sha256(question)] = decision;
+    emit Vote(sha256(name), sha256(question), decision);
   }
 
   function countQuestions() public view returns(uint256) {
